a52dbfa7862cdd173c366bfb26a36ffa8bc8569e
HaoF <hao50427126@gmail.com>
2023-06-08 18:49:53 -0700

    Added documentation to the project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Hao Fan
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Start the application
+     * @param args command-line args(not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
